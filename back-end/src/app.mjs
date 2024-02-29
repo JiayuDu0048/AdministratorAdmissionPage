9d344b6c11e6565617b6119eaa112d1d4f23eb5d
lesleyzhao <tz2074@nyu.edu>
2024-02-28 21:12:16 -0500

    Added back-end directory

---
+++ back-end/src/app.mjs
@@ -0,0 +1,62 @@
+import express from 'express';
+import url from 'url';
+import path from 'path';
+import multer from "multer";
+import cors from 'cors';
+import dotenv from 'dotenv';
+import "dotenv/config"
+import morgan from 'morgan';
+import session from 'express-session';
+import mongoose from 'mongoose';  
+import passport from 'passport'
+// import CustomJwtStrategy from './config/jwt-config.mjs';
+
+const app = express();
+
+
+// use the morgan middleware to log all incoming http requests
+app.use(morgan("dev"));
+
+// use express's builtin body-parser middleware to parse any data included in a request
+app.use(express.json()); // decode JSON-formatted incoming POST data
+app.use(express.urlencoded({ extended: true })); // decode url-encoded incoming POST data
+
+// serve static files
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+app.use("/static", express.static(path.join(__dirname, 'public')));
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+// cors
+const corsOptions = {
+  credentials: true,
+  origin: process.env.CLIENT_URL.replace(/\/$/, ""),
+  methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH']
+}
+app.use(cors(corsOptions));
+
+console.log(process.env.CLIENT_URL);
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB...');
+  })
+  .catch(err => console.error('Could not connect to MongoDB...', err));
+
+// session to auto-save user data (like id) when they login
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized:true,
+  cookie: {httpOnly: true, secure: process.env.NODE_ENV==="production"}
+}))
+console.log('Session secret:', process.env.SESSION_SECRET);
+
+// // jwt strategy
+// passport.use(CustomJwtStrategy)
+
+// // initialize passport
+// app.use(passport.initialize())
+
+
+export default app;
---
+++ back-end/src/server.mjs
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import server from "./app.mjs"
+
+// which port to listen for HTTP(S) requests
+const port = process.env.SERVER_PORT
+
+// call a function to start listening to the port
+const listener = server.listen(port, function () {
+  console.log(`Server listening on port: ${port}`)
+})
+
+// a function to stop listening to the port
+const close = () => {
+  listener.close()
+}
+
+export default close;
