0f4669d3580aa793443b987e13d0b4209b2db1c6
杜佳宇 <jiayudu@dujiayudeMacBook-Air.local>
2024-03-15 00:26:44 -0400

    add login page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,15 +1,31 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import React, { useEffect } from "react";
 import Navbar from "./Component/Navbar";
+import NavbarLogout from "./Component/NavbarLogout";
 import Table from "./Component/Table";
-
+import LoginPage from "./Component/LoginPage";
 function App() {
   return (
     <>
-      <Navbar />
-      <Table />
+      <div>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Navbar />} />
+            <Route path="/Table" element={<NavbarLogout />} />
+          </Routes>
+        </Router>
+        <div>
+          <Router>
+            <Routes>
+              <Route path="/" element={<LoginPage />} />
+              <Route path="/Table" element={<Table />} />
+            </Routes>
+          </Router>
+        </div>
+      </div>
     </>
   );
 }
