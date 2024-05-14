38391d2ce2ba8834e0c10d57bdf3b18c12484404
lesleyzhao <tz2074@nyu.edu>
2024-05-13 23:29:14 -0400

    Finish AI ChatBox & Auto-Bug-Reporting

--- front-end/src/utils/axiosConfig.js
+++ front-end/src/utils/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const serverURL = import.meta.env.VITE_SERVER_URL; // With Vite, and some other build tools, there's often a requirement for environment variables to be prefixed with VITE_ to be exposed to your project. 
-console.log(serverURL);
+console.log("ServerURL:", serverURL);
+
 const axiosProvider = axios.create({
     baseURL: serverURL,
     withCredentials: true,
